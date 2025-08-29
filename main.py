4ea1a4e258b739557b590b8b31a0f8372a5f3b1e
LucmanTree <pitifultree4002@gmail.com>
2025-08-29 09:07:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from pyscript import display 
 
-display("Welcome To Gnarly Dog Food!")
 
-restaurant_name = "Gnarly Dog Food"
+restaurant_name = ['Gnarly Dog Food'] #List
+owner_name = "The Man who forgot his name" #String
+year_established = 1872 #Integer
+popular_item_price = 45.00 #Floating Point
+has_delivery = False #Boolean
+product_names = ['The Doggy Bone','The Wagging Tail','Fluffy Fur'] #List
+business_hours = "7am-8pm" # This string
+menu_prices = {'20':'The Doggy Bone','24':'The Wagging Tail','40':'The Dog Collar','36':'the Pink Water Bowl','45':'Fluffy Fur'}  #ThisisDictionary
+common_allergens = 'Egg', 'Meat', 'Gluten' #This is a string
+tax_rate = 0.1 #This is an Integer
+
+display(year_established)
+display(common_allergens)
+display(tax_rate)
